d654185ee5f36388320bb7f4bf43bec1fadaa659
liuquansheng <15534386506@163.com>
2024-09-12 19:56:48 +0800

    fix the missing seg param

--- uer/targets/bilm_target.py
+++ uer/targets/bilm_target.py
@@ -26,9 +26,9 @@ class BilmTarget(LmTarget):
         tgt_forward, tgt_backward = tgt[0], tgt[1]
         # Forward.
         loss_forward, correct_forward, _ = \
-            self.lm(memory_bank[:, :, :self.hidden_size], tgt_forward)
+            self.lm(memory_bank[:, :, :self.hidden_size], tgt_forward, seg)
         # Backward.
         loss_backward, correct_backward, denominator_backward = \
-            self.lm(memory_bank[:, :, self.hidden_size:], tgt_backward)
+            self.lm(memory_bank[:, :, self.hidden_size:], tgt_backward, seg)
 
         return loss_forward, loss_backward, correct_forward, correct_backward, denominator_backward
